00c12fef9565587af904d19c14742aa627cb598e
Wagner <wagner.abr@hotmail.com>
2020-02-13 23:02:12 -0300

    Manipulando arquivos - metodos delete e update

--- 06-cli/database.js
+++ 06-cli/database.js
@@ -43,6 +43,45 @@ class Database {
         const dadosFiltrados = dados.filter(item => (id ? (item.id === id) : true))
         return dadosFiltrados
     }
+
+    async remover(id) {
+        if(!id) {
+            return await this.escreverArquivo([])
+        }
+        const dados = await this.obterDadosArquivo()
+        //findIndex permite passar uma function para dentro dele para que ele retorne boolean 
+        const indice = dados.findIndex(item => item.id === parseInt(id))
+        
+        if(indice === -1) {
+            throw Error('O usuário não existe')
+        }
+        dados.splice(indice, 1)
+        return await this.escreverArquivo(dados)
+    }
+
+    async atualizar(id, modificacoes) {
+        const dados = await this.obterDadosArquivo()
+        
+        const indice = dados.findIndex(item => item.id === parseInt(id))
+        
+        if(indice === -1) {
+            throw Error('Heroi não existe')
+        }
+        const atual = dados[indice]
+
+        const objetosAtualizar = {
+            ...atual,
+            ...modificacoes
+        }
+        //remove da lista
+        dados.splice(indice, 1)
+
+        return await this.escreverArquivo([
+            ...dados,
+            objetosAtualizar
+        ])
+        //return false
+    }
 }
 
 module.exports = new Database()
--- 06-cli/test.js
+++ 06-cli/test.js
@@ -11,9 +11,16 @@ const DEFAULT_ITEM_CADASTRAR = {
     id: 1
 }
 
+const DEFAULT_ITEM_ATUALIZAR = {
+    nome: 'Lanterna Verde',
+    poder: 'Energia do Anel',
+    id: 2
+}
+
 describe('Suite manipulação de herois', function() {
     before(async () => {
         await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+        await database.cadastrar(DEFAULT_ITEM_ATUALIZAR)
     })
     it('deve pesquisar um heroi usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR
@@ -27,6 +34,27 @@ describe('Suite manipulação de herois', function() {
         const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
         const [actual] = await database.listar(expected.id)
         deepEqual(actual, expected)
+    })
+    it('Deve remover um heroi por id', async () => {
+        const expected = true
+        const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id)
+        deepEqual(resultado, expected)
+    })
+    it('Deve atualizar um heroi por id', async () => {
+        const expected = { 
+            ...DEFAULT_ITEM_ATUALIZAR,
+            nome: 'Batman',
+            poder: 'Dinheiro'
+        }
+
+        const novoDado = { 
+            nome: 'Batman',
+            poder: 'Dinheiro'
+        }
+
+        await database.atualizar(DEFAULT_ITEM_ATUALIZAR.id, novoDado)
+        const [resultado] = await database.listar(DEFAULT_ITEM_ATUALIZAR.id)
+        deepEqual(resultado, expected)
     })
 }
 
